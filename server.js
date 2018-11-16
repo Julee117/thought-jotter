676415a2b38f96fe2d17e597183c1b618eedcd69
github username <juliannne@gmail.com>
2018-11-16 17:43:22 -0500

    Add routes, bodyparser and mongoose

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
+const routes = require("./routes/api");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost/notedb");
+mongoose.Promise = global.Promise;
+
+app.use(bodyParser.json());
+app.use(routes);
 
 app.listen(port, () => console.log("talk-to-notepad app started on: " + port));
