bec606a1fc23360a67768b8ce68246d222805d0a
github username <juliannne@gmail.com>
2018-11-20 12:25:01 -0500

    Modify functions

--- controllers/noteController.js
+++ controllers/noteController.js
@@ -1,49 +1,39 @@
 const Note = require("../models/note");
 
+exports.all_notes = function(req, res, next) {
+  Note.find({}).then(function(notes) {
+    res.send(notes);
+  }).catch(next);
+};
+
 exports.new = function(req, res, next) {
   let note = new Note();
   note.title = req.body.title;
   note.content = req.body.content;
 
-  note.save(function(err) {
-    if (err) {
-      return next(err);
-    }
+  note.save().then(function(note) {
     res.send(note);
-  })
+  }).catch(next);
 };
 
 exports.note_detail = function(req, res, next) {
-  Note.findById(req.params.id, function(err, note) {
-    if (err) {
-      return next(err);
-    }
+  Note.findById(req.params.id).then(function(note) {
     res.send(note);
-  })
+  }).catch(next);
 };
 
 exports.note_update = function(req, res, next) {
-  Note.findById(req.params.id, function(err, note) {
-    if (err) {
-      return next(err);
-    } else {
-      note.title = req.body.title;
-      note.content = req.body.content;
-      note.save(function(err) {
-        if (err) {
-          return next(err);
-        }
-        res.send(note);
-      })
-    }
-  })
+  Note.findById(req.params.id).then(function(note) {
+    note.title = req.body.title;
+    note.content = req.body.content;
+    note.save().then(function(note) {
+      res.send(note);
+    }).catch(next);
+  }).catch(next);
 };
 
 exports.note_delete = function(req, res, next) {
-  Note.findByIdAndRemove(req.params.id, function(err) {
-    if (err) {
-      return next(err);
-    }
-    res.send("Note deleted");
-  })
+  Note.findByIdAndRemove(req.params.id).then(function(note) {
+    res.send(note);
+  }).catch(next);
 }
