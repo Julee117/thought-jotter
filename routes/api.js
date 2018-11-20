1563be3a95201b776b444d5f22a5e652ad161978
github username <juliannne@gmail.com>
2018-11-20 12:24:46 -0500

    Create route to get list of notes

--- routes/api.js
+++ routes/api.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const noteController = require("../controllers/noteController");
 
+router.get("/notes", noteController.all_notes);
 router.post("/notes", noteController.new);
 router.get("/notes/:id", noteController.note_detail);
 router.put("/notes/:id", noteController.note_update);
