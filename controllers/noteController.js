87c713787da4aaeb07c1d532df7e36fa117ff53b
github username <juliannne@gmail.com>
2018-11-20 11:19:44 -0500

    Create update function

--- controllers/noteController.js
+++ controllers/noteController.js
@@ -21,3 +21,20 @@ exports.note_detail = function(req, res, next) {
     res.send(note);
   })
 };
+
+exports.note_update = function(req, res, next) {
+  Note.findById(req.params.id, function(err, note) {
+    if (err) {
+      return next(err);
+    } else {
+      note.title = req.body.title;
+      note.content = req.body.content;
+      note.save(function(err) {
+        if (err) {
+          return next(err);
+        }
+        res.send(note);
+      })
+    }
+  })
+};
--- routes/api.js
+++ routes/api.js
@@ -4,5 +4,6 @@ const noteController = require("../controllers/noteController");
 
 router.post("/notes", noteController.new);
 router.get("/notes/:id", noteController.note_detail);
+router.put("/notes/:id", noteController.note_update);
 
 module.exports = router;
