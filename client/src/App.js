e9e9fc3519d81ce63f8290ce1765f5692a3a8c40
github username <juliannne@gmail.com>
2018-12-10 23:39:29 -0500

    Add button

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,17 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+  handleOnClick = () => {
+    this.props.history.push('/notes')
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           Welcome to Thought Jotter
+          <button onClick={this.handleOnClick}>Click to start</button>
         </header>
       </div>
     );
