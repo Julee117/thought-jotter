af0c047fd5f3dab2a60601fc4b2efdaac93f77eb
github username <juliannne@gmail.com>
2018-12-10 23:37:12 -0500

    Add routes

--- client/src/index.js
+++ client/src/index.js
@@ -2,9 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import NotesPage from './NotesPage';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Router>
+    <Switch>
+      <Route path="/" exact component={App} />
+      <Route path="/notes" component={NotesPage} />
+    </Switch>
+  </Router>,
+  document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
