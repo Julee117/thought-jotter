50a51d9e73b67dadd19d09c7d2e639f8224b8f4a
github username <juliannne@gmail.com>
2018-12-13 15:26:28 -0500

    Create notespage

---
+++ client/src/NotesPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class NotesPage extends Component {
+  render() {
+    return (
+      <div>
+        Hello
+      </div>
+    )
+  }
+}
+
+export default NotesPage;
