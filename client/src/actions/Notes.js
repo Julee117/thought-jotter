67f74ff358aed932c26733a0177688e692ea3c64
github username <juliannne@gmail.com>
2018-12-13 18:09:23 -0500

    Add setNotes and getNotes

---
+++ client/src/actions/Notes.js
@@ -0,0 +1,17 @@
+const API_URL = process.env.REACT_APP_API_URL;
+
+const setNotes = notes => {
+  return {
+    type: 'GET_NOTES',
+    notes
+  }
+}
+
+export const getNotes = () => {
+  return dispatch => {
+    return fetch(`${API_URL}/notes`)
+      .then(response => response.json())
+      .then(notes => dispatch(setNotes(notes)))
+      .catch(error => console.log(error));
+  }
+}
