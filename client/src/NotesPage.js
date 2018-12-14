b1ce63bcf938002ae6764bbbb71ffdff6ce0616f
github username <juliannne@gmail.com>
2018-12-14 12:18:29 -0500

    Import bindActionCreators, getNotes, route, NoteForm

--- client/src/NotesPage.js
+++ client/src/NotesPage.js
@@ -1,20 +1,45 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { getNotes } from './actions/Notes';
+import { bindActionCreators } from 'redux';
+import { Route, Switch } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import NoteForm from './NoteForm';
 
 class NotesPage extends Component {
 
+  componentDidMount() {
+    this.props.getNotes();
+  }
+
   render() {
+    const { match, notes } = this.props;
+
     return (
       <div>
         <div>
           <Navbar />
         </div>
         <div>
-          Hello
+          <Switch>
+            <Route path={`${match.url}/new`} component={NoteForm} />
+          </Switch>
         </div>
       </div>
     )
   }
 };
 
-export default NotesPage;
+const mapStateToProps = state => {
+  return {
+    notes: state.notes
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators({
+    getNotes
+  }, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(NotesPage);
