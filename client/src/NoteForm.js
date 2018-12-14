9cdf4a1be3daea010684bf9499faed2e209716d2
github username <juliannne@gmail.com>
2018-12-14 13:07:34 -0500

    Modify form, add handleChange

--- client/src/NoteForm.js
+++ client/src/NoteForm.js
@@ -10,11 +10,37 @@ class NoteForm extends Component {
     }
   }
 
+  handleChange = event => {
+    const {name, value} = event.target
+    this.setState({
+      [name]: value
+    })
+  }
+
   render() {
     return (
       <div>
         <h1>Create new note</h1>
         <form>
+          <div>
+            <label htmlFor="title">Title:</label>
+            <input
+              type="text"
+              onChange={this.handleChange}
+              name="title"
+              value={this.state.title}
+            />
+          </div>
+          <div>
+            <label htmlFor="content">Content:</label>
+            <textarea
+              type="text"
+              onChange={this.handleChange}
+              name="content"
+              value={this.state.content}
+            />
+          </div>
+          <button type="submit">Submit</button>
         </form>
       </div>
     )
