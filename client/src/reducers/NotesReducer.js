ca8248c3fe10c0c01e4da086337b9280bd5c64e5
github username <juliannne@gmail.com>
2018-12-19 18:11:13 -0500

    Add CREATE_NOTE

--- client/src/reducers/NotesReducer.js
+++ client/src/reducers/NotesReducer.js
@@ -2,6 +2,8 @@ export default (state = [], action) => {
   switch(action.type) {
     case 'GET_NOTES':
       return action.notes;
+    case 'CREATE_NOTE':
+      return state.concat(action.note);
     default:
       return state;
   }
