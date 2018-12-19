a63bd4d078a5d46b594901ed13fe3423eabfbc49
github username <juliannne@gmail.com>
2018-12-19 18:12:50 -0500

    Add createNote

--- client/src/actions/Notes.js
+++ client/src/actions/Notes.js
@@ -7,11 +7,33 @@ const setNotes = notes => {
   }
 }
 
+const addNote = note => {
+  return {
+    type: 'CREATE_NOTE',
+    note
+  }
+}
+
 export const getNotes = () => {
   return dispatch => {
     return fetch(`${API_URL}notes`)
       .then(response => response.json())
       .then(notes => dispatch(setNotes(notes)))
       .catch(error => console.log(error));
+  }
+}
+
+export const createNote = note => {
+  return dispatch => {
+    return fetch(`${API_URL}notes`, {
+      method: "POST",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify({
+        title: note.title,
+        content: note.content}),
+    })
+      .then(response => response.json())
+      .then(note => dispatch(addNote(note)))
+      .catch(error => console.log(error))
   }
 }
