7c558a253c3d60f61fe64256f9b20e889e49e4de
github username <juliannne@gmail.com>
2018-12-19 18:35:23 -0500

    Add rootReducer

--- client/src/index.js
+++ client/src/index.js
@@ -5,11 +5,11 @@ import App from './App';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
-import notesReducer from './reducers/NotesReducer';
+import rootReducer from './reducers/index';
 import * as serviceWorker from './serviceWorker';
 
 const store = createStore(
-  notesReducer,
+  rootReducer,
   compose(
     applyMiddleware(thunk),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
