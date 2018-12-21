1266394c69a094d2e46cec2d0fa2609287efdc5d
github username <juliannne@gmail.com>
2018-12-21 00:14:23 -0500

    Add DELETE_NOTE

--- client/src/reducers/NotesReducer.js
+++ client/src/reducers/NotesReducer.js
@@ -4,6 +4,9 @@ export default (state = [], action) => {
       return action.notes;
     case 'CREATE_NOTE':
       return state.concat(action.note);
+    case 'DELETE_NOTE':
+      const notes = state.filter(note => note._id !== action.note._id);
+      return notes
     default:
       return state;
   }
