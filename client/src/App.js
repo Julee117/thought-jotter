1f5181515ee1e23d75936e67c30bf693904ae939
github username <juliannne@gmail.com>
2018-12-28 15:20:44 -0500

    Import Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
-import HomePage from './HomePage';
-import NotesPage from './containers/NotesPage';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
+import Navbar from './components/Navbar';
 
 class App extends Component {
-
-  render() {
+  render(){
     return (
       <div>
-        <Router>
-          <Switch>
-            <Route exact path="/" component={HomePage} />
-            <Route path="/notes" component={NotesPage} />
-          </Switch>
-        </Router>
+        <Navbar history={this.props.history}/>
+        {this.props.children}
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default withRouter(App);
