c4f8591f883f07ce1f4d5530b39620c2108f345e
github username <juliannne@gmail.com>
2018-12-28 12:10:02 -0500

    Add routes and requireAuth

---
+++ routes/notes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const noteController = require('../controllers/noteController');
+const passport = require('passport');
+const passportService = require('../config/passport');
+
+const requireAuth = passport.authenticate('jwt', {session: false});
+
+router.get("/notes", requireAuth, noteController.all_notes);
+router.post("/notes", requireAuth, noteController.new);
+router.get("/notes/:id", requireAuth, noteController.note_detail);
+router.delete("/notes/:id", requireAuth, noteController.note_delete);
+
+module.exports = router;
