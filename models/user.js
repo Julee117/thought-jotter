14ba7223092505e97512860ad2a6288a227dd97e
github username <juliannne@gmail.com>
2018-12-28 12:12:48 -0500

    Create UserSchema

---
+++ models/user.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const { isEmail } = require('validator');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  email: {
+    type: String,
+    required: true,
+    validate: [isEmail, "Invalid email"],
+    lowercase: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
+
+UserSchema.methods.generateHash = function(password) {
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+  };
+
+UserSchema.methods.validPassword = function(password) {
+  return bcrypt.compareSync(password, this.password);
+};
+
+const User = mongoose.model("user", UserSchema);
+
+module.exports = User;
