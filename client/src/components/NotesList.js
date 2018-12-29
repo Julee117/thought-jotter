0b75fdbdaacc9e898422a906acf1232f718d5041
github username <juliannne@gmail.com>
2018-12-29 10:57:52 -0500

    Add Container, Row, Col

--- client/src/components/NotesList.js
+++ client/src/components/NotesList.js
@@ -1,18 +1,26 @@
 import React from 'react';
 import NoteCard from './NoteCard';
 import { Link } from 'react-router-dom';
+import { Container, Row, Col } from 'reactstrap';
 
 const NotesList = ({notes}) => {
   const noteCards = notes.map(note =>
-    <Link key={note._id} to={`/notes/${note._id}`}>
-      <NoteCard note={note} />
-    </Link>
+    <Col md="4">
+      <Col md="12" className="noteCard">
+        <Link key={note._id} to={`/notes/${note._id}`} style={{textDecoration: 'none'}}>
+          <NoteCard note={note} />
+        </Link>
+      </Col>
+    </Col>
   );
 
   return(
     <div>
-      <h1>Notes</h1>
-      {noteCards}
+      <Container>
+        <Row className="justify-content-around">
+          {noteCards}
+        </Row>
+      </Container>
     </div>
   )
 }
