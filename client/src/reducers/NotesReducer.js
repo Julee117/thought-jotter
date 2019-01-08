a079f5e351f5da5486f253128c323f8c467723a9
github username <juliannne@gmail.com>
2019-01-08 14:33:48 -0500

    Add UPDATE_NOTE

--- client/src/reducers/NotesReducer.js
+++ client/src/reducers/NotesReducer.js
@@ -4,9 +4,12 @@ export default (state = [], action) => {
       return action.notes;
     case 'CREATE_NOTE':
       return state.concat(action.note);
+    case 'UPDATE_NOTE':
+      const index = state.findIndex(item => item._id === action.note._id)
+      return [...state.slice(0, index), action.note, ...state.slice(index + 1)]
     case 'DELETE_NOTE':
       const notes = state.filter(note => note._id !== action.note._id);
-      return notes
+      return notes;
     default:
       return state;
   }
