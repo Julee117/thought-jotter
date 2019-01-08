41da36aa6b67df19364d211e99e72ccbcfa69f30
github username <juliannne@gmail.com>
2019-01-08 14:39:01 -0500

    Modify spacing

--- client/src/components/NotesList.js
+++ client/src/components/NotesList.js
@@ -7,7 +7,7 @@ const NotesList = ({notes}) => {
   const noteCards = notes.map(note =>
     <Col key={note._id} md="4">
       <Col md="12" className="noteCard">
-        <Link  to={`/notes/${note._id}`} style={{textDecoration: 'none'}}>
+        <Link to={`/notes/${note._id}`} style={{textDecoration: 'none'}}>
           <NoteCard note={note} />
         </Link>
       </Col>
