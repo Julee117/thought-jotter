efda8f22b86aa14ba77218ebc9424e27536c2acb
github username <juliannne@gmail.com>
2019-01-08 14:35:48 -0500

    Modify newLines, add edit button

--- client/src/containers/NoteShow.js
+++ client/src/containers/NoteShow.js
@@ -5,22 +5,35 @@ import { deleteNote } from '../actions/Notes';
 import { Container, Button } from 'reactstrap';
 
 class NoteShow extends Component {
+
   handleOnClick = () => {
     this.props.deleteNote(this.props.note)
     this.props.history.push('/notes')
   }
 
+  handleEditClick = () => {
+    this.props.history.push(`/notes/${this.props.note._id}/edit`)
+  }
+
   render() {
     const {note} = this.props;
-    const newLines = (note.content).split("\n").map((item, key) => {
-      return <span key={key}>{item}<br/></span>
-    })
+    const newLines = () => {
+      const match = /\r|\n/.exec(note.content);
+      if (match) {
+        return note.content.split("\n").map((item, key) => {
+          return <span key={key}>{item}<br/></span>
+        })
+      } else {
+        return <p>{note.content}</p>
+      }
+    }
 
     return(
       <Container>
         <div className="note col-lg-6">
           <h3 className="title">{note.title}</h3>
-          {newLines}
+          {newLines()}
+          <Button onClick={this.handleEditClick} outline color="success" size="sm">Edit</Button>
           <Button onClick={this.handleOnClick} outline color="danger" size="sm" className="deleteButton">Delete</Button>
         </div>
       </Container>
