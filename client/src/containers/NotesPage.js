51e700e8588428ec4b83b59696113042af9c0041
github username <juliannne@gmail.com>
2019-01-08 14:40:17 -0500

    Add edit route

--- client/src/containers/NotesPage.js
+++ client/src/containers/NotesPage.js
@@ -6,6 +6,7 @@ import { Route, Switch } from 'react-router-dom';
 import NoteForm from './NoteForm';
 import NotesList from '../components/NotesList';
 import NoteShow from './NoteShow';
+import EditNoteForm from './EditNoteForm';
 
 class NotesPage extends Component {
 
@@ -19,6 +20,7 @@ class NotesPage extends Component {
       <div>
         <Switch>
           <Route path={`${match.url}/new`} component={NoteForm} />
+          <Route path={`${match.url}/:id/edit`} component={EditNoteForm} />
           <Route path={`${match.url}/:id`} component={NoteShow} />
           <Route exact path={`${match.url}`} render={() => <NotesList notes={notes} />} />
         </Switch>
